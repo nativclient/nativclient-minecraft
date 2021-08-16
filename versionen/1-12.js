45dbf6d2288a33acc88942340fc0bb523a8c5503
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-08-17 00:53:00 +0200

    [+] Version 1.12

---
+++ versionen/1-12.js
@@ -0,0 +1,10 @@
+client
+{
+  sourceCompatibility = MinecraftVersion.VERSION_1_12
+  targetCompatibility = MinecraftVersion.VERSION_1_12
+};
+
+plugins
+{
+  local bath_path/system/plugins/1_12 *
+};
