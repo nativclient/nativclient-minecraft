76ea6c584a4a1948eebbfaa8bddf89b996f5b046
AmethystexDEV <89048709+AmethystexDEV@users.noreply.github.com>
2021-08-17 01:47:53 +0200

    [+] Add OptiFine-Import

---
+++ user/plugins/optifine_latest.js
@@ -0,0 +1,20 @@
+function(optifine_1.8.8)
+{
+  
+  import url(https://optifine.net/downloadx?f=OptiFine_1.8.8_HD_U_I7.jar&x=2d9c6472168a07db16cfb258901086d9)
+  
+};
+
+function(optifine_1.12)
+{
+  
+  import url(https://optifine.net/downloadx?f=OptiFine_1.12_HD_U_G5.jar&x=3f1b2bdde2128c274864cc49cd78eb68)
+  
+};
+
+function(optifine_1.17)
+{
+  
+  import url(https://optifine.net/downloadx?f=preview_OptiFine_1.17_HD_U_G9_pre26.jar&x=3f1b2bdde2128c274864cc49cd78eb68)
+  
+};
