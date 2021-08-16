eba5fff4c3f5777575b08de6bc080ea63c28a1fa
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-08-17 00:56:38 +0200

    [+] Installer JS

---
+++ system/scripts/installer.js
@@ -0,0 +1,8 @@
+// Imports
+import * from url(https://nativclient/source/minecraft/getInstall$token);
+
+// Code
+if import "echo = 100%"
+   touch file.permission
+                  grant permission = \a\m
+end;
