0cf0e6cb06328b671c62f44dea7101b0ad845ec3
whiteHai1 <84030867+whiteHai1@users.noreply.github.com>
2021-08-17 18:09:21 +0200

    [+] code.js erstellt

---
+++ user/addon/status/code.js
@@ -0,0 +1,15 @@
+//Import 
+local import config.js
+
+//Code 
+if user status = abwesend
+execute function(status_abwesend)
+end;
+
+if user status = online
+execute function(status_online)
+end;
+
+if user status = nicht_stören
+execute function(status_nicht_stören)
+end;
