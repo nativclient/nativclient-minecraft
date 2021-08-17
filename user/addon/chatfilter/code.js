95a71af3ad67bfb1372e4f8bec7d274f7fa6805f
whiteHai1 <84030867+whiteHai1@users.noreply.github.com>
2021-08-17 14:32:05 +0200

    [:] Function fixed

--- user/addon/chatfilter/code.js
+++ user/addon/chatfilter/code.js
@@ -1,6 +1,11 @@
-// Imports
-
 // Configs
 local import config.js
 
 // Code
+if @Minecraft_sys_chat get(update)
+    @Minecraft_sys_chat('message') == local function(chatfilter_words)
+              touch @Minecraft_sys_chat('message')
+              {
+                message_color = local function(chatfilter_color)
+              };
+end;
