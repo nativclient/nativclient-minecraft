d145f73aca0a7b3f36ae065623b1cdc764dad142
whiteHai1 <84030867+whiteHai1@users.noreply.github.com>
2021-08-17 14:57:40 +0200

    [+] code.js erstellt

---
+++ user/addon/chaticons/code.js
@@ -0,0 +1,40 @@
+//Import 
+chaticon_window = import window(bath_path/overlay/chaticons.js)
+
+//Code
+add new @Minecraft_sys_chatlist
+    {
+      hidden_text = "nativclient_chaticons"
+      enabled = true
+      active = false
+    }
+end;
+
+// Open und Close Funktions
+function(open)
+{
+  touch line(9)
+  {
+    active = true
+  }
+};
+
+function(close)
+{
+  touch line(9)
+  {
+    active = false
+  }
+};
+
+
+// Chatwindow Icons öffnen und schließe
+function(open_chaticon_window)
+{
+  onclick open.chaticon_window
+};
+
+function(close_chaticon_window)
+{
+  onclick close.chaticon_window
+}
