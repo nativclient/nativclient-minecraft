8638114e5a6a1d43fc87e1debd740d09dcad75c0
AmethystexDEV <89048709+AmethystexDEV@users.noreply.github.com>
2021-08-17 02:33:48 +0200

    [:] Edit Ränge

--- system/plugins/*/rangsystem.js
+++ system/plugins/*/rangsystem.js
@@ -8,5 +8,26 @@ name="Rangtag"
 end;
 
 touch file(Rangtag)
-display $user_rang
+display "Native " $user_rang
+end;
+
+
+if rang = "Admin"
+color = dark_red
+end;
+
+if rang = "Dev"
+color = #09B1CF
+end;
+
+if rang = "Sup"
+color = #0012EE
+end;
+
+if rang = "Freund"
+color = #01CD11
+end;
+
+if rang = "Spieler"
+color = #797979
 end;
