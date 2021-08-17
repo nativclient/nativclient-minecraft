61c1d548e4a07b54e1a292eab069876863c00b84
whiteHai1 <84030867+whiteHai1@users.noreply.github.com>
2021-08-17 19:57:23 +0200

    [:] Fix #30

--- system/db/db1.sql
+++ system/db/db1.sql
@@ -1,11 +1,16 @@
+<php
 $SQLGetServerInfos = $db->prepare("SELECT * FROM `user_settings` WHERE `id` = :userid");
 $SQLGetServerInfos->execute(array(":userid" => $user_id));
 $serverInfos = $SQLGetServerInfos -> fetch(PDO::FETCH_ASSOC);
 
-define(rang) = <?= $serverInfos['rang']; ?>
-define(user) = <?= $serverInfos['user']; ?>
-define(friends) = <?= $serverInfos['friends']; ?>
-define(banned) = <?= $serverInfos['banned']; ?>
-define(amount) = <?= $serverInfos['amount']; ?>
-define(cosmetics) = <?= $serverInfos['cosmetics']; ?>
-define(status) = <?= $serverInfos['status']; ?>
+rang = $serverinfos['rang']; 
+user = $serverinfos['user']; 
+friends = $serverinfos['friends']; 
+banned = $serverinfos['banned']; 
+amount = $serverinfos['amount']; 
+cosmetics = $serverinfos['cosmetics'];
+status = $serverinfos['status']; 
+
+
+
+?>
