a12e93a53c075d1298ff8bc822bbf67cee190201
whiteHai1 <84030867+whiteHai1@users.noreply.github.com>
2021-08-17 18:15:35 +0200

    [+] Status config.js

---
+++ user/addon/status/config.js
@@ -0,0 +1,14 @@
+function(status_abwesend)
+{
+  display img.url(https://cdn.discordapp.com/emojis/801013167546171452.gif?v=1)
+};
+
+function(status_online)
+{
+  display img.url(https://cdn.discordapp.com/emojis/801013167035383808.gif?v=1)
+};
+
+function(status_nicht_stören)
+{
+  display img.url(https://discord.com/assets/2ff27b392104b9f10c0faa72f68e3fbd.svg)
+}
