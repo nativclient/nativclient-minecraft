632b9e2e4efe6d2689f352f899ab9100c15080f8
Alex L <89048709+AmethystexDEV@users.noreply.github.com>
2021-08-17 23:28:25 +0200

    [:] Added FPS-Boost

--- user/plugins/fps_boost_alle_mods.js
+++ user/plugins/fps_boost_alle_mods.js
@@ -1,6 +1,22 @@
-function(betterfps-1.10.2)
+function(dynamicfps-1.17.1)
 {
   
-  import url(https://www.curseforge.com/minecraft/mc-mods/betterfps/download)
+  import url(https://www.curseforge.com/minecraft/mc-mods/dynamic-fps/download)
+  
+}
+
+
+function(betterfps-1.12.1)
+{
+  
+  import url(https://www.curseforge.com/minecraft/mc-mods/betterfps/download/2469845)
+  
+}
+
+
+function(betterfps-1.8.9)
+{
+  
+  import url(https://www.curseforge.com/minecraft/mc-mods/betterfps/download/2271480)
   
 }
