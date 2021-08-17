b83be138d41b30bd406ebcc68a27f88e82d44d13
whiteHai1 <84030867+whiteHai1@users.noreply.github.com>
2021-08-17 18:04:17 +0200

    [:] Es wurden mehrere imports gemacht.

--- system/db/db1.sql
+++ system/db/db1.sql
@@ -3,3 +3,8 @@ $SQLGetServerInfos->execute(array(":userid" => $user_id));
 $serverInfos = $SQLGetServerInfos -> fetch(PDO::FETCH_ASSOC);
 
 define(rang) = <?= $serverInfos['rang']; ?>
+define(user) = <?= $serverInfos['user']; ?>
+define(friends) = <?= $serverInfos['friends']; ?>
+define(banned) = <?= $serverInfos['banned']; ?>
+define(amount) = <?= $serverInfos['amount']; ?>
+define(cosmetics) = <?= $serverInfos['cosmetics']; ?>
