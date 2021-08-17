b477a0ddbfdee6a96a876087d1b26eb4429c03b0
Alex L <89048709+AmethystexDEV@users.noreply.github.com>
2021-08-17 23:12:44 +0200

    [+] Added OptiFine-Version

---
+++ user/plugins/optifine_settings.js
@@ -0,0 +1,8 @@
+// Imports
+db = local import ../../system/db/db1.sql
+local import optifine_latest.js
+
+// Code
+if $user local db.tables `optifine_settings` = true
+   function(optifine_$user_version)
+end;
