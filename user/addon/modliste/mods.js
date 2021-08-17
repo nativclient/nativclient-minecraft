2fd051af05a19ae2a2fe7f80eadc60239607ffb4
Alex L <89048709+AmethystexDEV@users.noreply.github.com>
2021-08-17 23:52:39 +0200

    [+] Added Mods

---
+++ user/addon/modliste/mods.js
@@ -0,0 +1,33 @@
+// Imports
+db = local import ../../system/db/db1.sql
+local import mod_imports.js
+
+// Code
+if $user local db.tables `hide_armor-1_17_1` = true
+   function(hide_armor-1.17.1)
+end;
+
+
+if $user local db.tables `custom_crosshair-1_17_1` = true
+   function(custom_crosshair-1.17.1)
+end;
+
+
+if $user local db.tables `item_physics-1_12_2` = true
+   function(item_physics-1.12.2)
+end;
+
+
+if $user local db.tables `custom_crosshair-1_12` = true
+   function(custom_crosshair-1.12)
+end;
+
+
+if $user local db.tables `item_physics-1_8_9` = true
+   function(item_physics-1.8.9)
+end;
+
+
+if $user local db.tables `spotify-1_8_9` = true
+   function(spotify-1.8.9)
+end;
