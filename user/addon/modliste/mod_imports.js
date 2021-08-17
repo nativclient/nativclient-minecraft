bcf37db7e07d241baab662ee368f08277e5a3e11
Alex L <89048709+AmethystexDEV@users.noreply.github.com>
2021-08-17 23:52:19 +0200

    [+] Added Mods

--- user/addon/modliste/mod_imports.js
+++ user/addon/modliste/mod_imports.js
@@ -1,6 +1,46 @@
-function(dynamicfps-1.17.1)
+function(hide_armor-1.17.1)
 {
   
-  import url(https://www.curseforge.com/minecraft/mc-mods/dynamic-fps/download)
+  import url(https://www.curseforge.com/minecraft/mc-mods/hide-armor/download)
+  
+}
+
+
+function(custom_crosshair-1.17.1)
+{
+  
+  import url(https://www.curseforge.com/minecraft/mc-mods/custom-crosshair-mod/download/3383474)
+  
+}
+
+
+function(item_physics-1.12.2)
+{
+  
+  import url(https://www.curseforge.com/minecraft/mc-mods/itemphysic/download/2903368)
+  
+}
+
+
+function(custom_crosshair-1.12)
+{
+  
+  import url(https://www.curseforge.com/minecraft/mc-mods/custom-crosshair-mod/download/2448808)
+  
+}
+
+
+function(item_physics-1.8.9)
+{
+  
+  import url(https://minecraft-inside.com/engine/download.php?id=126)
+  
+}
+
+
+function(spotify-1.8.9)
+{
+  
+  import url(https://www.curseforge.com/minecraft/mc-mods/spotify-viewer/download/3153379)
   
 }
