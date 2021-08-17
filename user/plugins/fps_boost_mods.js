8e7d845e162209a56f555021818d5cbedaddf93e
Alex L <89048709+AmethystexDEV@users.noreply.github.com>
2021-08-17 23:28:33 +0200

    [:] Added FPS-Boost

--- user/plugins/fps_boost_mods.js
+++ user/plugins/fps_boost_mods.js
@@ -3,6 +3,16 @@ db = local import ../../system/db/db1.sql
 local import fps_boost_alle_mods.js
 
 // Code
-if $user local db.tables `fps_boost_mods` = true
-   function()
+if $user local db.tables `dynamicfps-1_17_1` = true
+   function(dynamicfps-1.17.1)
+end;
+
+
+if $user local db.tables `betterfps-1_12_1` = true
+   function(betterfps-1.12.1)
+end;
+
+
+if $user local db.tables `betterfps-1_8_9` = true
+   function(betterfps-1.8.9)
 end;
