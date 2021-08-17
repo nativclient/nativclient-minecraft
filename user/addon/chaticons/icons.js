371c85d8e724dc3808343ce6a0d4fd2c09c7ab81
whiteHai1 <84030867+whiteHai1@users.noreply.github.com>
2021-08-17 17:06:36 +0200

    [:] Loading Icon hinzugefügt

--- user/addon/chaticons/icons.js
+++ user/addon/chaticons/icons.js
@@ -1,5 +1,6 @@
 function(import_icons)
 {
+  icon_loading = url(https://www.google.com/url?sa=i&url=https%3A%2F%2Ftenor.com%2Fsearch%2Floading-gifs&psig=AOvVaw0nBPLQjz5qSl6Fnl8MqcyT&ust=1629299144932000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCPiB97qquPICFQAAAAAdAAAAABAD),
   icon_nativclient = url(https://iconsplace.com/wp-content/uploads/_icons/ff0000/256/png/nfc-n-icon-14-256.png),
   icon_lachen = url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.appannie.com%2Fde%2Fapps%2Fgoogle-play%2Fapp%2Fcom.shareitagain.bigemoji%2F&psig=AOvVaw2dtk8eHKGPgUp46QxHYAdF&ust=1629291600783000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCKi9-LKOuPICFQAAAAAdAAAAABAS);
 }
