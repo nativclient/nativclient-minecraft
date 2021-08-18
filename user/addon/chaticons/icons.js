10bba1ca838ec55df736652b1b87305906ce609e
whiteHai1 <84030867+whiteHai1@users.noreply.github.com>
2021-08-18 18:11:01 +0200

    [:] Mehrere Icons hinzugefügt.

--- user/addon/chaticons/icons.js
+++ user/addon/chaticons/icons.js
@@ -2,5 +2,18 @@ function(import_icons)
 {
   icon_loading = url(https://www.google.com/url?sa=i&url=https%3A%2F%2Ftenor.com%2Fsearch%2Floading-gifs&psig=AOvVaw0nBPLQjz5qSl6Fnl8MqcyT&ust=1629299144932000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCPiB97qquPICFQAAAAAdAAAAABAD),
   icon_nativclient = url(https://iconsplace.com/wp-content/uploads/_icons/ff0000/256/png/nfc-n-icon-14-256.png),
-  icon_lachen = url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.appannie.com%2Fde%2Fapps%2Fgoogle-play%2Fapp%2Fcom.shareitagain.bigemoji%2F&psig=AOvVaw2dtk8eHKGPgUp46QxHYAdF&ust=1629291600783000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCKi9-LKOuPICFQAAAAAdAAAAABAS);
+  icon_pssht = url(https://discord.com/assets/acb7b1a237378bbddd34927b4001b8f4.svg),
+  icon_grining = url(https://discord.com/assets/7c010dc6da25c012643ea22c1f002bb4.svg),
+  icon_smiley = url(https://discord.com/assets/66f6c781fe86c346fbaf3390618668fc.svg),
+  icon_smile = url(https://discord.com/assets/626aaed496ac12bbdb68a86b46871a1f.svg),
+  icon_sweatlachen = url(https://discord.com/assets/b45af785b0e648fe2fb7e318a6b8010c.svg),
+  icon_joy = url(https://discord.com/assets/6201503f3aa918470a2190b36d1e196f.svg),
+  icon_rofl = url(https://discord.com/assets/567230faa19ec889fad5613630597049.svg),
+  icon_relaxed = url(https://discord.com/assets/b426f77f42bb709bcb2faa23602d2dc4.svg),
+  icon_blusch = url(https://discord.com/assets/6f75b58eb746d08c2c18451573876292.svg),
+  icon_innocent = url(https://discord.com/assets/e461bb3847973d47822fdc79d66f494d.svg),
+  icon_slightsmile = url(https://discord.com/assets/da3651e59d6006dfa5fa07ec3102d1f3.svg),
+  icon_upsidedown = url(https://discord.com/assets/559c3311dcdb3f23b7fb745559207db9.svg),
+  icon_wink = url(https://discord.com/assets/2e41bfdeba797283ee9da9bb439c3ece.svg),
+  icon_lachen = url(https://discord.com/assets/cb91d362742dcc886e4a4c3f981f3365.svg);
 }
