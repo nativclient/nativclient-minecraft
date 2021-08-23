4c7c1daee7daa08bfca155fe0d144605c9a6c644
Alex L <89048709+AmethystexDE@users.noreply.github.com>
2021-08-23 20:13:09 +0200

    [:] Added shader-import

--- user/addon/shaderliste/shader_import.js
+++ user/addon/shaderliste/shader_import.js
@@ -1,1 +1,54 @@
-Hallo
+function(kudar_shader-1.8)
+{
+  
+  import url(https://www.mediafire.com/file/stwyz8u89eivvq6/kuda-shaders-v6.5.26.zip/file)
+  
+}
+
+
+function(kudar_shader-1.12)
+{
+  
+  import url(https://www.mediafire.com/file/yoweov259dt5bub/KUDA-Shaders_v6.1_Legacy.zip/file)
+  
+}
+
+
+function(kudar_shader-1.17.1)
+{
+  
+  import url(https://www.mediafire.com/file/yoweov259dt5bub/KUDA-Shaders_v6.1_Legacy.zip/file)
+  
+}
+
+
+function(chocapic_shader_high)
+{
+  
+  import url(https://cloud01.black-host.eu/index.php/s/nDaYPjgAbAefQY5/download/Chocapic13%20V6%20High.zip)
+  
+}
+
+
+function(chocapic_shader_extreme)
+{
+  
+  import url(https://cloud01.black-host.eu/index.php/s/Tx2FbeAPwjHdnfo/download/Chocapic13%20V6%20Extreme.zip)
+  
+}
+
+
+function(sildur_vibrant_shader_high)
+{
+  
+  import url(https://download1484.mediafire.com/4nbd4obu7x9g/2p9tn2fult48hny/Sildur%5C%27s+Vibrant+Shaders+v1.29+High.zip)
+  
+}
+
+
+function(sildur_vibrant_shader_extreme)
+{
+  
+  import url(https://download1596.mediafire.com/3t871dewndlg/7p4mtkw1xqqxnb2/Sildur%5C%27s+Vibrant+Shaders+v1.29+Extreme.zip)
+  
+}
