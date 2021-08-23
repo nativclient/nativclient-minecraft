b8cf415089a9fbcd7b4bfae0810b4aa1b35bd676
Alex L <89048709+AmethystexDE@users.noreply.github.com>
2021-08-23 21:40:35 +0200

    [:] playtimer

--- user/plugins/playtimer.js
+++ user/plugins/playtimer.js
@@ -2,3 +2,35 @@
 local time = $time
 local game = ../../nativmc.exe
 local logs = ../../system/logsystem.js
+
+
+// Maincode
+if game is 
+   started = true
+   touch line 14 rewrite "timer = true"
+end
+
+
+// Settings
+timer = false
+
+
+// Timer Code
+function(timer)
+{
+  if timer = true
+    get current_time
+    name = "start-time"
+    if game is
+       started = false
+       get current_time
+       name = "end-time"
+     end
+   end
+}
+
+execute logs
+     write.file
+     name = "Spiellog $current_time"
+     text = "$start-time - $end-time"
+end
