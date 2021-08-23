f00ae9e4fa5b6500f58258179df311dc5ecfd4d4
Alex L <89048709+AmethystexDE@users.noreply.github.com>
2021-08-23 20:13:25 +0200

    [:] Edit shader

--- user/addon/shaderliste/shaders.js
+++ user/addon/shaderliste/shaders.js
@@ -1,1 +1,79 @@
-Hallo
+// Imports
+db = local import ../../system/db/db1.sql
+local import shader_imports.js
+
+
+// Code
+if $user local db.tables `kudar_shader-1_8` = true
+   function(kudar_shader-1.8)
+end;
+
+
+if $user local db.tables `kudar_shader-1_12` = true
+   function(kudar_shader-1.12)
+end;
+
+
+if $user local db.tables `kudar_shader-1_17` = true
+   function(kudar_shader-1.17.1)
+end;
+
+
+if $user local db.tables `chocapic_shader_high-1_8` = true
+   function(chocapic_shader_high)
+end;
+
+
+if $user local db.tables `chocapic_shader_high-1_12` = true
+   function(chocapic_shader_high)
+end;
+
+
+if $user local db.tables `chocapic_shader_high-1_17` = true
+   function(chocapic_shader_high)
+end;
+
+
+if $user local db.tables `chocapic_shader_extreme-1_8` = true
+   function(chocapic_shader_extreme)
+end;
+
+
+if $user local db.tables `chocapic_shader_extreme-1_12` = true
+   function(chocapic_shader_extreme)
+end;
+
+
+if $user local db.tables `chocapic_shader_extreme-1_17` = true
+   function(chocapic_shader_extreme)
+end;
+
+
+if $user local db.tables `sildur_vibrant_shader_high-1_8` = true
+   function(sildur_virbrant_shader_high)
+end;
+
+
+if $user local db.tables `sildur_vibrant_shader_high-1_12` = true
+   function(sildur_virbrant_shader_high)
+end;
+
+
+if $user local db.tables `sildur_vibrant_shader_high-1_17` = true
+   function(sildur_virbrant_shader_high)
+end;
+
+
+if $user local db.tables `sildur_vibrant_shader_extreme-1_8` = true
+   function(sildur_virbrant_shader_extreme)
+end;
+
+
+if $user local db.tables `sildur_vibrant_shader_extreme-1_12` = true
+   function(sildur_virbrant_shader_extreme)
+end;
+
+
+if $user local db.tables `sildur_vibrant_shader_extreme-1_17` = true
+   function(sildur_virbrant_shader_extreme)
+end;
