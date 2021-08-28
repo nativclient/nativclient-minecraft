d51b0636469f3f3cbbd095d3795f14b710abd99e
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-08-28 14:04:15 +0200

    [:] Added (#61)

--- user/plugins/selfmade/voicechat.js
+++ user/plugins/selfmade/voicechat.js
@@ -1,6 +1,30 @@
 // Imports
 $voiceserv_nmbr = firstexecute(voiceserv)
 
+local muted_icon import = url(https://image.flaticon.com/icons/png/512/66/66928.png)
+local notspeaking_icon import = url(https://image.flaticon.com/icons/png/512/66/66928.png)
+local speaking_icon import = url(https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Speaker_Icon.svg/500px-Speaker_Icon.svg.png)
+
+// Neues Objekt
+if $user `voicechat` = false
+    set speeK_texture = local muted_icon
+
+elseif $user `voicechat` = true AND $user_object_mic get input
+    set speeK_texture = local speaking_icon
+
+elseif $user `voicechat` = true AND $user_object_mic get input = false
+    set speeK_texture = local notspeaking_icon
+
+end
+
+create new(object)
+{
+   name = "voicechat_icon"
+   loc = x=100 y=29 z=38
+   texture = speek_texture
+}
+end
+
 // Maincode
 if $user `voicechat` = true
    execute function(voicechat)
