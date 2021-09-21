7b62667d5387544963c6c37cba34e019c14455de
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-09-21 21:27:50 +0200

    [:] Fixed overwrite

--- minecraft_write_over.js
+++ minecraft_write_over.js
@@ -1,3 +1,18 @@
-onexecute installer.exe
-!  load * from local .minecraft into user .minecraft
+// !!!old!!!
+
+//onexecute installer.exe
+//!  load * from local .minecraft into user .minecraft
+//end
+
+// !!!old!!!
+
+if installer.exe['execute'] = true
+   function(overwrite)
 end
+
+function(overwrite)
+{
+  select * from .minecraft copy(clipboard)
+  paste(clipboard) = $user_minecraft
+  continue = true
+}
