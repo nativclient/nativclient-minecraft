3cc5ac3285960287bc23f9ba51f1efde74934ab7
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-10-19 00:55:49 +0200

    [:] Fix: version change

--- .minecraft/autoupdater.js
+++ .minecraft/autoupdater.js
@@ -7,4 +7,9 @@ function(update)
   minecraft.get-version.$version
 }
 
+if minecraft.version < '1.6', == '1.7', == '1.9', == '1.10', == '1.11', == '1.13', == '1.14', == '1.15', == '1.17', == '1.18'
+   minecraft.stop
+   echo 'Diese Version wird nicht unterstützt'
+end
+
 // alpha
