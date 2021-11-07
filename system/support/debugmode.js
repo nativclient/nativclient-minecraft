dfaa54a04605474cf5081cd0a835b13808a8caf7
Coding Base Updater <93847688+SB-CodingBase@users.noreply.github.com>
2021-11-07 02:05:06 +0100

    Repairing the code 👨‍🔧

--- system/support/debugmode.js
+++ system/support/debugmode.js
@@ -8,13 +8,14 @@ if base_path/system config.js
    debugmode = true
    touch line(3)
           change("execute=true")
+   end;
 end;
 
 // File
 
 if $execute = true
    open base_path/system logger.js
-   write file
+   echo file
    {
      catch($error);
    } 
