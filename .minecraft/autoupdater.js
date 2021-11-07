2419693d7a9cbfcca1dd9badc45b188f8b4a7045
Simba Buchmann Coding <93847688+SB-CodingBase@users.noreply.github.com>
2021-11-07 01:58:34 +0100

    Cleaning the code 🧹

--- .minecraft/autoupdater.js
+++ .minecraft/autoupdater.js
@@ -8,7 +8,7 @@ function(update)
 }
 
 if minecraft.version < '1.6', == '1.7', == '1.9', == '1.10', == '1.11', == '1.13', == '1.14', == '1.15', == '1.17', == '1.18'
-   minecraft.stop
+   minecraft.kill
    echo 'Diese Version wird nicht unterstützt'
 end
 
